f3e599967aa28b2c19daad49895c2a5b668d7311
DESKTOP-O1U24RQ\Kevin_Yu <jsyu17@gmail.com>
2021-09-13 20:11:09 +0800

    update

--- LogdnaExtensions.cs
+++ LogdnaExtensions.cs
@@ -1,13 +1,80 @@
 using System;
 using System.Net;
 using Serilog.Configuration;
+using Serilog.Core;
 using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Sinks.Http;
+using Serilog.Sinks.Http.BatchFormatters;
+using Serilog.Sinks.Http.Private.Network;
+using Serilog.Sinks.Http.Private.Sinks;
+using Serilog.Sinks.Http.TextFormatters;
 using Serilog.Sinks.LogDNA;
 
 namespace Serilog
 {
     public static class LogdnaExtensions
     {
+        public static LoggerConfiguration HttpLogDna(this LoggerSinkConfiguration sinkConfiguration, string apiKey,
+            string appName = null,
+            string commaSeparatedTags = null,
+            string requestUri = "https://logs.logdna.com/logs/ingest",
+            int batchPostingLimit = 1000,
+            int? queueLimit = null,
+            TimeSpan? period = null,
+            ITextFormatter textFormatter = null,
+            IBatchFormatter batchFormatter = null,
+            LogEventLevel restrictedToMinimumLevel = LogEventLevel.Verbose)
+        {
+            if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
+            if (string.IsNullOrWhiteSpace(apiKey)) throw new ArgumentNullException(nameof(apiKey));
+            if (string.IsNullOrWhiteSpace(requestUri)) throw new ArgumentNullException(nameof(requestUri));
+
+            requestUri += (requestUri.Contains("?") ? "&" : "?") + $"hostname={Dns.GetHostName().ToLower()}";
+            if (commaSeparatedTags != null) requestUri += $"&tags={WebUtility.UrlEncode(commaSeparatedTags)}";
+
+            var envName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ??
+                          Environment.GetEnvironmentVariable("ASPNET_ENVIRONMENT");
+            if (sinkConfiguration == null)
+                throw new ArgumentNullException(nameof(sinkConfiguration));
+            period = (period ?? TimeSpan.FromSeconds(2.0));
+            textFormatter = textFormatter ?? new LogdnaTextFormatter(appName ?? "unknown", envName);
+            batchFormatter = batchFormatter ?? new LogdnaBatchFormatter();
+            IHttpClient httpClient = new LogdnaHttpClient(apiKey);
+            return sinkConfiguration.Http(requestUri, batchPostingLimit, queueLimit.Value, period.Value,
+              textFormatter, batchFormatter, restrictedToMinimumLevel, httpClient);
+
+        }
+        public static LoggerConfiguration DurableHttpLogDna(
+            this LoggerSinkConfiguration sinkConfiguration,
+            string apiKey,
+            string appName = null,
+            string commaSeparatedTags = null,
+            string requestUri = "https://logs.logdna.com/logs/ingest",
+            string bufferPathFormat = "Buffer-{Date}.json",
+            long? bufferFileSizeLimitBytes = null,
+            int? retainedBufferFileCountLimit = 31,
+            int batchPostingLimit = 1000,
+            TimeSpan? period = null,
+            ITextFormatter textFormatter = null,
+            IBatchFormatter batchFormatter = null,
+            LogEventLevel restrictedToMinimumLevel = LogEventLevel.Verbose)
+        {
+            if (sinkConfiguration == null)
+                throw new ArgumentNullException(nameof(sinkConfiguration));
+
+            requestUri += (requestUri.Contains("?") ? "&" : "?") + $"hostname={Dns.GetHostName().ToLower()}";
+            if (commaSeparatedTags != null) requestUri += $"&tags={WebUtility.UrlEncode(commaSeparatedTags)}";
+
+            var envName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ??
+                          Environment.GetEnvironmentVariable("ASPNET_ENVIRONMENT");
+            period = new TimeSpan?(period ?? TimeSpan.FromSeconds(2.0));
+            textFormatter = textFormatter ?? new LogdnaTextFormatter(appName ?? "unknown", envName);
+            batchFormatter = batchFormatter ?? new LogdnaBatchFormatter();
+            IHttpClient httpClient = new LogdnaHttpClient(apiKey);
+            return sinkConfiguration.DurableHttp(requestUri, bufferPathFormat, bufferFileSizeLimitBytes, retainedBufferFileCountLimit, batchPostingLimit, period.Value, textFormatter, batchFormatter, httpClient);
+        }
+
         public static LoggerConfiguration LogDNA(
             this LoggerSinkConfiguration sinkConfiguration,
             string apiKey,
@@ -26,7 +93,7 @@ namespace Serilog
             ingestUrl += (ingestUrl.Contains("?") ? "&" : "?") + $"hostname={Dns.GetHostName().ToLower()}";
             if (commaSeparatedTags != null) ingestUrl += $"&tags={WebUtility.UrlEncode(commaSeparatedTags)}";
 
-            var envName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? 
+            var envName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ??
                 Environment.GetEnvironmentVariable("ASPNET_ENVIRONMENT");
 
             return sinkConfiguration.Http(ingestUrl,
@@ -51,7 +118,7 @@ namespace Serilog
 
             var envName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ??
                           Environment.GetEnvironmentVariable("ASPNET_ENVIRONMENT");
-            var bufferMode = "Buffer-{"+ (DurableBufferMode)logDNAConfiguration.BufferMode+ "}.json";
+            var bufferMode = "Buffer-{" + (DurableBufferMode)logDNAConfiguration.BufferMode + "}.json";
             return sinkConfiguration.DurableHttp(logDNAConfiguration.IngestUrl,
                 bufferPathFormat: bufferMode,
                 batchPostingLimit: logDNAConfiguration.BatchPostingLimit,
@@ -70,7 +137,7 @@ namespace Serilog
             int? batchPostingLimit = null,
             TimeSpan? period = null,
             LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
-            DurableBufferMode bufferMode= DurableBufferMode.Hour
+            DurableBufferMode bufferMode = DurableBufferMode.Hour
           )
         {
             if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
@@ -79,7 +146,7 @@ namespace Serilog
 
             ingestUrl += (ingestUrl.Contains("?") ? "&" : "?") + $"hostname={Dns.GetHostName().ToLower()}";
             if (commaSeparatedTags != null) ingestUrl += $"&tags={WebUtility.UrlEncode(commaSeparatedTags)}";
-   
+
             var envName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ??
                           Environment.GetEnvironmentVariable("ASPNET_ENVIRONMENT");
             return sinkConfiguration.DurableHttp(ingestUrl,
@@ -88,10 +155,10 @@ namespace Serilog
                 period: period ?? TimeSpan.FromSeconds(15),
                 textFormatter: new LogdnaTextFormatter(appName ?? "unknown", envName),
                 batchFormatter: new LogdnaBatchFormatter(),
-                restrictedToMinimumLevel:restrictedToMinimumLevel,
+                restrictedToMinimumLevel: restrictedToMinimumLevel,
                 httpClient: new LogdnaHttpClient(apiKey));
         }
     }
 
-  
+
 }
